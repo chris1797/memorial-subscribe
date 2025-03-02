4cea25be206073b9fd7bd964fde16bd76e0033dd
chris1797 <jaehun6004@gmail.com>
2025-03-02 18:29:42 +0900

    setup

---
+++ src/main/java/memorial/core/MemorialSubscribeApplication.java
@@ -0,0 +1,13 @@
+package memorial.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorialSubscribeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemorialSubscribeApplication.class, args);
+    }
+
+}
