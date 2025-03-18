c304cec74e838884a34d1a2c250a09a486d9ec2f
chris1797 <jaehun6004@gmail.com>
2025-03-18 23:30:17 +0900

    test: memorialDead 저장 관련 테스트코드 추가

--- src/test/java/memorial/core/domain/memorial/MemorialServiceTest.java
+++ src/test/java/memorial/core/domain/memorial/MemorialServiceTest.java
@@ -1,39 +1,52 @@
 package memorial.core.domain.memorial;
 
 import jakarta.transaction.Transactional;
+import memorial.core.api.request.MemorialDeadDto;
 import memorial.core.api.request.MemorialRequestDto;
+import memorial.core.common.enums.MemberGrade;
 import memorial.core.domain.member.Member;
 import memorial.core.domain.member.MemberRepository;
+import memorial.core.domain.memorialDead.MemorialDead;
+import memorial.core.domain.memorialDead.MemorialDeadRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MemorialServiceTest {
 
+    @Autowired private MemorialRepository memorialRepository;
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private MemorialDeadRepository memorialDeadRepository;
+
     // Test Fixtures
-    static MemorialRequestDto requestDto;
+    static MemorialDeadDto memorialDeadDto;
+    static MemorialRequestDto memorialRequestDto;
     static Member testMember;
-
-    @Autowired
-    private MemorialRepository memorialRepository;
-
-    @Autowired
-    private MemberRepository memberRepository;
 
 
     @BeforeAll
     static void setUp() {
         // Test Fixtures 세팅
-        requestDto = new MemorialRequestDto(
+        memorialDeadDto = new MemorialDeadDto(
+                "deadName",
+                LocalDate.of(1970, 1, 1),
+                LocalDate.of(2025, 1, 1)
+        );
+
+        memorialRequestDto = new MemorialRequestDto(
                 1L,
                 "test",
                 false,
-                false);
+                false,
+                memorialDeadDto);
 
         Member member = new Member();
         member.setName("testMember");
@@ -42,19 +55,40 @@ class MemorialServiceTest {
     }
 
 
+    @Transactional
     @DisplayName("추모관 저장 테스트")
     @Test
+    @Rollback(false)
     void memorialSaveTestWithMember() {
         // given
+        testMember.setMemberGrade(MemberGrade.FREE);
         Member savedMember = memberRepository.save(testMember);
-        Memorial memorial = Memorial.of(requestDto, savedMember);
+        Memorial memorial = Memorial.of(memorialRequestDto, savedMember);
+        MemorialDead memorialDead = MemorialDead.of(memorialDeadDto, memorial);
 
         // when
         Memorial saveEntity = memorialRepository.save(memorial);
+        MemorialDead savedMemorialDead = memorialDeadRepository.save(memorialDead);
 
         // then
         assertNotNull(saveEntity);
+        assertNotNull(savedMemorialDead);
         assertEquals(savedMember.getId(), memorial.getMember().getId());
+    }
+
+    @Transactional
+    @DisplayName("FREE 회원 추모관 저장 사전검사 테스트")
+    @Test
+    @Rollback(false)
+    void freeMemberMakeMemorialFailTest() {
+        // given
+        testMember.setMemberGrade(MemberGrade.FREE);
+
+        // when
+        boolean canMakeMemorial = testMember.canMakeMemorial();
+
+        // then
+        assertFalse(canMakeMemorial);
     }
 
 }
